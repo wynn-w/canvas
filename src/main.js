e34c368a7373a88b5bfa808b52089d67b8252066
wynn-w <wwwynn22@gmail.com>
2024-06-30 18:20:33 +0800

    feat: implementation of shape dragging

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ class CanvasControl {
     this.canvasInit(ops);
     this.store = new Map([["circle", []]]);
     this.activeTarget = null;
+    this.status = {
+      status: GraStatusEnum.IDLE,
+    };
   }
   canvasInit(ops) {
     this.canvas = document.getElementById(ops.container);
@@ -38,30 +41,36 @@ class CanvasControl {
     };
   }
 }
+const GraStatusEnum = {
+  IDLE: 0,
+  DRAG_START: 1,
+  DRAGGING: 2,
+};
 const entry = new CanvasControl({
   container: "canvas-main",
 });
 const canvas = entry.canvas;
 
 class Circle {
-  constructor(ops = {}) {
-    const { x = 100, y = 100, r = 50 } = ops;
-    this.meta = { x, y, r };
-    this.create();
+  constructor(ops = { x: 100, y: 100, r: 50, lineWidth: 2 }) {
+    this.meta = { ...ops };
+    this.draw();
   }
-  create() {
+  draw() {
     entry.ctx.save();
     entry.ctx.beginPath();
-    const { x, y, r } = this.meta;
+    const { x, y, r, lineWidth } = this.meta;
     entry.ctx.strokeStyle = "red";
     entry.ctx.arc(x, y, r, 0, 360);
+    entry.ctx.lineWidth = lineWidth;
     entry.ctx.stroke();
 
     entry.ctx.closePath();
     entry.ctx.restore();
     entry.store.get("circle").push({
-      meta: { x, y, r },
+      meta: this.meta,
       hot: true,
+      pointer: this,
     });
   }
 }
@@ -71,5 +80,45 @@ const c1 = new Circle();
 canvas.addEventListener("mousedown", (e) => {
   const pos = entry.getPointerPosition(e);
   const target = entry.findTarget(pos);
-  console.log(target);
+  if (target) {
+    entry.status.status = GraStatusEnum.DRAG_START;
+    entry.activeTarget = target;
+    entry.lastPos = pos;
+  }
+});
+canvas.addEventListener("mousemove", (e) => {
+  const pos = entry.getPointerPosition(e);
+  if (entry.findTarget(pos)) {
+    canvas.style.cursor = "all-scroll";
+  } else {
+    canvas.style.cursor = "";
+  }
+  if (
+    entry.status.status === GraStatusEnum.DRAG_START &&
+    entry.getDistance(pos, entry.lastPos) > 5 
+  ) {
+    console.log("ready to drag");
+    entry.status.status = GraStatusEnum.DRAGGING;
+  } else if (entry.status.status === GraStatusEnum.DRAGGING) {
+    console.assert(entry.activeTarget, "not exist");
+    if (entry.activeTarget) {
+      console.log("dragging");
+      entry.status.status = GraStatusEnum.DRAGGING;
+
+      // clear screen -> TODO: clear target
+      entry.ctx.clearRect(0, 0, entry.canvas.width, entry.canvas.height);
+
+      entry.activeTarget.meta.x = pos.x;
+      entry.activeTarget.meta.y = pos.y;
+
+      // redraw target shape
+      entry.activeTarget.pointer.draw();
+    }
+  }
+});
+canvas.addEventListener("mouseup", (e) => {
+  if (entry.status.status === GraStatusEnum.DRAGGING) {
+    entry.status.status = GraStatusEnum.IDLE;
+    console.log("drag finished");
+  }
 });
